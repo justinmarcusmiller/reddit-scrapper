26896b4e9b0782fb2c1ba8b515e37441a05439fb
Justin Miller <justinmiller171@gmail.com>
2020-09-08 02:24:27 -0700

    added craigslist search and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node.js Web Scrapper
+
+Node.js web scrapper built with the cheerio.js library
+
+Displays items for sale on craigslist
+
+## Usage
+
+node index.js --city --seach-term
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const { argv } = require('process');
+let city = process.argv[2];
+let searchTerm = process.argv[3];
 
-const url = 'https://www.imdb.com/';
+const url = `https://${city}.craigslist.org/search/sss?query=${searchTerm}&sort=date`;
+
+if (process.argv[2] == null || process.argv[3] == null) {
+  console.log("INVALID ARGUMENTS");
+  console.log("node index.js city searchTerm");
+  process.exit();
+}
 
 function getResults() {
   return fetch(url)
@@ -11,8 +20,8 @@ function getResults() {
 getResults()
   .then(body => {
     const $ = cheerio.load(body);
-    $('.ipc-poster-card__title').each(function(i, element) {
+    $('.result-title').each(function(i, element) {
       const $element = $(element);
-      console.log($element.text())
+      console.log($('.result-title').eq(i).text() + " | " + $('.result-meta').eq(i).find('.result-price').text())
     })
   })
